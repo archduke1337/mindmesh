c511462bd4805df13d2dfd20df4d56a8fc7ebb34
Gaurav <shresthramyadav@gmail.com>
2025-11-11 01:53:00 +0530

    Update appwrite.ts

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -40,7 +40,7 @@ export const authService = {
   // Login
   async login(email: string, password: string) {
     try {
-      return await (account as any).createEmailPasswordSession(email, password);
+      return await account.createEmailPasswordSession(email, password);
     } catch (error) {
       throw error;
     }
